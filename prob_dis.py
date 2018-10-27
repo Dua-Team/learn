71e031b85c1516c9c60e444772a95e1c401d950e
hkchekc <31205016+hkchekc@users.noreply.github.com>
2018-10-27 10:26:42 +0200

    Update prob_dis.py

--- prob_dis.py
+++ prob_dis.py
@@ -1,3 +1,15 @@
 import numpy as np
 import scipy
 import matplotlib as mpl
+
+def ndis(theta = 0.5, episilon = 0.1, beta=np.arange(0, 1, 0.1, float)):
+    norm = ss.norm
+    x = norm(theta,episilon)
+    pdf = x.pdf(beta)
+    return pdf
+
+f= ndis(theta=0.5, episilon=0.15)
+g= ndis(theta=0.5,episilon=0.2)
+
+mlr = g/f
+print(mlr)
